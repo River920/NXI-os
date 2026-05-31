16890c083e637116b8317e787cdb0bfcd3466bba
River920 <river.m.phoenix@gmail.com>
2026-05-31 19:55:15 +1000

    Added kernel, wont print Q

--- bootloader.asm
+++ bootloader.asm
@@ -1,59 +1,90 @@
-[org 0x7c00]
+[org 0x7c00]                        
+KERNEL_LOCATION equ 0x1000
+                                    
+
+mov [BOOT_DISK], dl                 
+
+                                    
+xor ax, ax                          
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, KERNEL_LOCATION
+mov dh, 2
+
+mov ah, 0x02
+mov al, dh 
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13                ; no error management, do your homework!
+
+                                    
+mov ah, 0x0
+mov al, 0x3
+int 0x10                ; text mode
 
 
-mov [BOOT_DISK], dl
-
-
-
-CODE_SEG equ code_descriptor - GDT_Start
-DATA_SEG equ data_descriptor - GDT_Start
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
 
 cli
-lgdt [GDT_Descriptor]
+lgdt [GDT_descriptor]
 mov eax, cr0
 or eax, 1
 mov cr0, eax
 jmp CODE_SEG:start_protected_mode
 
 jmp $
+                                    
+BOOT_DISK: db 0
 
+GDT_start:
+    GDT_null:
+        dd 0x0
+        dd 0x0
 
-GDT_Start:
-    null_descriptor:
-        dd 0
-        dd 0
-
-    code_descriptor:
+    GDT_code:
         dw 0xffff
-        dw 0
-        db 0
-        db 10011010b
-        db 11001111b
-        db 0
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
 
-    data_descriptor:
+    GDT_data:
         dw 0xffff
-        dw 0
-        db 0
-        db 10010010b
-        db 11001111b
-        db 0
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
 
-GDT_End:
+GDT_end:
 
-GDT_Descriptor:
-    dw GDT_End - GDT_Start - 1
-    dd GDT_Start
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
 
 
 [bits 32]
 start_protected_mode:
-    mov al, 'A'
-    mov ah, 0x0f ; white on black
-    mov [0xb8000], ax
-    jmp $
+    mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ebp, 0x90000		; 32 bit stack base pointer
+	mov esp, ebp
 
-BOOT_DISK: db 0
+    jmp KERNEL_LOCATION
 
-times 510-($-$$) db 0
+                                     
+ 
+times 510-($-$$) db 0              
 dw 0xaa55
--- compile.sh
+++ compile.sh
@@ -1,5 +1,17 @@
 clear
 
 nasm -f bin bootloader.asm -o bootloader.bin
-qemu-system-x86_64 bootloader.bin
 
+gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "kernel.o"
+
+nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
+
+nasm "zeroes.asm" -f bin -o "zeroes.bin"
+
+ld -e main -m elf_i386 -s -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o"
+
+nasm "bootloader.asm" -f bin -o "bootloader.bin"
+
+cat "bootloader.bin" "full_kernel.bin" > "everything.bin"
+
+cat "everything.bin" "zeroes.bin" > "OS.bin"
