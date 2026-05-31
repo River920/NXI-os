6d966863b3151356b49db4cc8aabc2d419aafbcc
River920 <river.m.phoenix@gmail.com>
2026-05-31 14:45:10 +1000

    Made it print the alphabet

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,4 +1,18 @@
-jmp $
+mov ah, 0x0e
+mov al, 'a'
+int 0x10
+
+
+loop:
+    inc al
+    cmp al, 'z' + 1
+    je exit
+    int 0x10
+    jmp loop
+
+
+exit:
+    jmp $
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
