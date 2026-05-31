19084a4925a4d90c6ae172bedbddf800be363f6b
River920 <river.m.phoenix@gmail.com>
2026-05-31 17:28:29 +1000

    made it go into protected mode

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,23 +1,59 @@
 [org 0x7c00]
 
-mov ah, 0x0e
-mov bx, test
 
-printString:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp printString
+mov [BOOT_DISK], dl
 
 
-test:
-    db "Testing this hopefully it works", 0
 
-end:
+CODE_SEG equ code_descriptor - GDT_Start
+DATA_SEG equ data_descriptor - GDT_Start
+
+cli
+lgdt [GDT_Descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+
+jmp $
+
+
+GDT_Start:
+    null_descriptor:
+        dd 0
+        dd 0
+
+    code_descriptor:
+        dw 0xffff
+        dw 0
+        db 0
+        db 10011010b
+        db 11001111b
+        db 0
+
+    data_descriptor:
+        dw 0xffff
+        dw 0
+        db 0
+        db 10010010b
+        db 11001111b
+        db 0
+
+GDT_End:
+
+GDT_Descriptor:
+    dw GDT_End - GDT_Start - 1
+    dd GDT_Start
+
+
+[bits 32]
+start_protected_mode:
+    mov al, 'A'
+    mov ah, 0x0f ; white on black
+    mov [0xb8000], ax
     jmp $
 
+BOOT_DISK: db 0
 
 times 510-($-$$) db 0
 dw 0xaa55
