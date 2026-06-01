b16afd280e2f7b71a3bf564549c636846733e57c
River920 <river.m.phoenix@gmail.com>
2026-06-01 11:26:30 +1000

    made folders neater

--- compile.sh
+++ compile.sh
@@ -1,19 +1,15 @@
 clear
 
-nasm -f bin bootloader.asm -o bootloader.bin
+nasm -f bin bootloader/bootloader.asm -o output/bootloader/bootloader.bin
 
-i386-elf-gcc -ffreestanding -c "kernel.cpp" -o "kernel.o"
+i386-elf-gcc -ffreestanding -c "kernel/kernel.cpp" -o "output/kernel/kernel.o"
 
-nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
+nasm "kernel/kernel_entry.asm" -f elf -o "output/kernel/kernel_entry.o"
 
-nasm "zeroes.asm" -f bin -o "zeroes.bin"
+nasm "kernel/zeroes.asm" -f bin -o "output/kernel/zeroes.bin"
 
-i386-elf-ld -m elf_i386 -s -Ttext 0x1000 --oformat binary -o "full_kernel.bin" "kernel_entry.o" "kernel.o"
+i386-elf-ld -m elf_i386 -s -Ttext 0x1000 --oformat binary -o "output/kernel/full_kernel.bin" "output/kernel/kernel_entry.o" "output/kernel/kernel.o"
 
-nasm "bootloader.asm" -f bin -o "bootloader.bin"
+cat "output/bootloader/bootloader.bin" "output/kernel/full_kernel.bin" "output/kernel/zeroes.bin" > "output/os/os.bin"
 
-cat "bootloader.bin" "full_kernel.bin" > "everything.bin"
-
-cat "everything.bin" "zeroes.bin" > "OS.bin"
-
-qemu-system-x86_64 -drive format=raw,file="OS.bin",index=0,if=floppy,  -m 128M
+qemu-system-x86_64 -drive format=raw,file="output/os/os.bin",index=0,if=floppy,  -m 128M
