17c81725c82fb9a37340eb5ede11e03f63cb2547
River920 <river.m.phoenix@gmail.com>
2026-06-01 14:43:00 +1000

    freeze point

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,13 +1,27 @@
-extern "C" void main() {
-    char* video_memory = (char*) 0xB8000;
 
-    const char* kernelload = "Kernel loaded!";
+extern "C" void _start() {
+
+    const char* kernelload = "Kernel loaded! ";
+
+
+    char* video_memory = (char*) 0xB8000;
 
     int i = 0;
 
     while (kernelload[i] != '\0') {
-        video_memory[i*2] = kernelload[i];
-        video_memory[i*2 + 1] = 0x07;
-        ++i;
+    video_memory[i*2] = kernelload[i];
+    video_memory[i*2 + 1] = 0x07;
+    ++i;
     }
+
+    i = 0;
+
+
+
 }
+
+
+
+
+
+
