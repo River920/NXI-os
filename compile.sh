c168d25083f7ff77fa01108aaf5047a4ca2b37dc
River920 <river.m.phoenix@gmail.com>
2026-06-01 10:47:41 +1000

    installed i386 cross compiler and fixed scripts

--- compile.sh
+++ compile.sh
@@ -2,13 +2,13 @@ clear
 
 nasm -f bin bootloader.asm -o bootloader.bin
 
-gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "kernel.o"
+i386-elf-g++ -ffreestanding -c "kernel.cpp" -o "kernel.o"
 
 nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
 
 nasm "zeroes.asm" -f bin -o "zeroes.bin"
 
-ld -e main -m elf_i386 -s -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o"
+i386-elf-ld -m elf_i386 -s -Ttext 0x1000 --oformat binary -o "full_kernel.bin" "kernel_entry.o" "kernel.o"
 
 nasm "bootloader.asm" -f bin -o "bootloader.bin"
 
