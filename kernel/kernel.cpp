aee426da8d290ca012a015f0883a3e37fcab46b2
River920 <river.m.phoenix@gmail.com>
2026-06-01 13:21:02 +1000

    made it print a whole string

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,4 +1,13 @@
-extern "C" void main(){
-    *(char*)0xb8000 = 'Q';
-    return;
+extern "C" void main() {
+    char* video_memory = (char*) 0xB8000;
+
+    const char* kernelload = "Kernel loaded!";
+
+    int i = 0;
+
+    while (kernelload[i] != '\0') {
+        video_memory[i*2] = kernelload[i];
+        video_memory[i*2 + 1] = 0x07;
+        ++i;
+    }
 }
