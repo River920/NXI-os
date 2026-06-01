fa3dee797ca3a19b81757efa7df6fcc83475d7e2
River920 <river.m.phoenix@gmail.com>
2026-06-01 20:07:00 +1000

    trying to make it do \n

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,7 +1,7 @@
 
 extern "C" void _start() {
 
-    const char* kernelload = "Kernel loaded! ";
+    const char* kernelload = "Kernel loaded - NXI os";
 
 
     char* video_memory = (char*) 0xB8000;
@@ -9,13 +9,12 @@ extern "C" void _start() {
     int i = 0;
 
     while (kernelload[i] != '\0') {
-    video_memory[i*2] = kernelload[i];
-    video_memory[i*2 + 1] = 0x07;
-    ++i;
+        video_memory[i*2] = kernelload[i];
+        video_memory[i*2 + 1] = 0x07;
+        ++i;
     }
 
     i = 0;
-
 
 
 }
