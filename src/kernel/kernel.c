e3437e0f304cd2ee081698ccd48cebe1aed6bab4
River920 <river.m.phoenix@gmail.com>
2026-09-19 10:20:00 +1000

    Using itoa on BOOT_MEM_AMOUNT now

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -44,8 +44,7 @@ void _start(int BOOT_MEM_AMOUNT) {
     print("Kernel loaded", 1);
     print("Welcome to NXI os kernel", 3);
 
-    int i = 12;
-    const char* test = itoa(i, 10);
+    const char* MEM_AMOUNT_CHAR = itoa(BOOT_MEM_AMOUNT, 10);
 
     msleep(5000);// sleep 3 sec here
     cls(); // clear screen
@@ -56,9 +55,7 @@ void _start(int BOOT_MEM_AMOUNT) {
     msleep(1000);
     enable_cursor(0,3); // parameters change cursors size
     print("Cursor enabled", 2);
-    print(test, 3);
-    // print(BOOT_MEM_AMOUNT, 3); doesnt work, need itoa for this
-    
+    print(MEM_AMOUNT_CHAR, 3); // using itoa to properly print the mempory amount
 
 }
 
